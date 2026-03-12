f8822dfb92a8c7d472f9bab5efb15dd588a23148
Mikhail Bondarev <56403477+MIhai123555@users.noreply.github.com>
2026-03-12 22:38:34 +0300

    Add files via upload
    Prototype of the program, cant process images yet, but already able to identify at least one cluster in a given array

---
+++ ships.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct queue_node{
+    int x, y;
+    struct queue_node *next;
+} queue_node;
+
+typedef struct{
+    queue_node *beg_p, *end_p;
+} queue;
+
+void init_queue(queue *q_p){
+    q_p->beg_p = q_p->end_p = NULL;
+    return;
+}
+
+void push_queue(queue *q_p, int x, int y){
+    queue_node *new = (queue_node*)malloc(sizeof(queue_node));
+    new->x = x;
+    new->y = y;
+    new->next = NULL;
+    if (q_p->end_p) q_p->end_p->next = new;
+    q_p->end_p = new;
+    if (!q_p->beg_p) q_p->beg_p = new;
+    return;
+}
+
+int is_empty_queue(queue *q_p){
+    return (q_p->beg_p == NULL);
+}
+
+void pop_queue(queue *q_p, int *x_out, int *y_out){
+    queue_node *tmp = q_p->beg_p;
+    *x_out = tmp->x;
+    *y_out = tmp->y;
+    q_p->beg_p = tmp->next;
+    if (!q_p->beg_p) q_p->end_p = NULL;
+    free(tmp);
+    return;
+}
+
+void clear_queue(queue *q_p){
+    queue_node *tmp = q_p->beg_p;
+    while (q_p->beg_p){
+        q_p->beg_p = q_p->beg_p->next;
+        free(tmp);
+        tmp = q_p->beg_p;
+    }
+    q_p->end_p = NULL;
+    return;
+}
+
+void print_queue(queue *q_p){
+    queue_node *tmp = q_p->beg_p;
+    while (tmp){
+        printf("(%d %d) ", tmp->x, tmp->y);
+        tmp = tmp->next;
+    }
+    printf("\n");
+    return;
+}
+
+void scoop_the_blob(double **mat_brightness, int **mat_is_visited, double brightness_threshold, int *blob_size, int blob_number, int x, int y, int width, int height){
+    queue Queue, *queue_ptr = &Queue;
+    int cur_x, cur_y;
+    init_queue(queue_ptr);
+    push_queue(queue_ptr, x, y);
+    mat_is_visited[x][y] = 1;
+    blob_size[blob_number]++;
+    while (!is_empty_queue(queue_ptr)){
+        pop_queue(queue_ptr, &cur_x, &cur_y);
+        //printf("(%d %d)\n", cur_x, cur_y);
+        if (cur_y > 0 && !mat_is_visited[cur_x][cur_y-1] && mat_brightness[cur_x][cur_y-1] > brightness_threshold){
+            push_queue(queue_ptr, cur_x, cur_y-1);
+            blob_size[blob_number]++;
+            mat_is_visited[cur_x][cur_y-1] = 1;
+        }
+        if (cur_y > 0 && cur_x < width-1 && !mat_is_visited[cur_x+1][cur_y-1] && mat_brightness[cur_x+1][cur_y-1] > brightness_threshold){
+            push_queue(queue_ptr, cur_x+1, cur_y-1);
+            blob_size[blob_number]++;
+            mat_is_visited[cur_x+1][cur_y-1] = 1;
+        }
+        if (cur_x < width-1 && !mat_is_visited[cur_x+1][cur_y] && mat_brightness[cur_x+1][cur_y] > brightness_threshold){
+            push_queue(queue_ptr, cur_x+1, cur_y);
+            blob_size[blob_number]++;
+            mat_is_visited[cur_x+1][cur_y] = 1;
+        }
+        if (cur_x < width-1 && cur_y < height-1 && !mat_is_visited[cur_x+1][cur_y+1] && mat_brightness[cur_x+1][cur_y+1] > brightness_threshold){
+            push_queue(queue_ptr, cur_x+1, cur_y+1);
+            blob_size[blob_number]++;
+            mat_is_visited[cur_x+1][cur_y+1] = 1;
+        }
+        if (cur_y < height-1 && !mat_is_visited[cur_x][cur_y+1] && mat_brightness[cur_x][cur_y+1] > brightness_threshold){
+            push_queue(queue_ptr, cur_x, cur_y+1);
+            blob_size[blob_number]++;
+            mat_is_visited[cur_x][cur_y+1] = 1;
+        }
+        if (cur_x > 0 && cur_y < height-1 && !mat_is_visited[cur_x-1][cur_y+1] && mat_brightness[cur_x-1][cur_y+1] > brightness_threshold){
+            push_queue(queue_ptr, cur_x-1, cur_y+1);
+            blob_size[blob_number]++;
+            mat_is_visited[cur_x-1][cur_y+1] = 1;
+        }
+        if (cur_x > 0 && !mat_is_visited[cur_x-1][cur_y] && mat_brightness[cur_x-1][cur_y] > brightness_threshold){
+            push_queue(queue_ptr, cur_x-1, cur_y);
+            blob_size[blob_number]++;
+            mat_is_visited[cur_x-1][cur_y] = 1;
+        }
+    }
+    return;
+}
+
+int main(void){
+    int width, height, last_pixel = width*height, i, j, blob_number = 0, ptr = 0,
+    *blob_size = (int*)calloc(10000, sizeof(int));
+    scanf("%d%d", &width, &height);
+    double *mat_brightness[height];
+    int *mat_is_visited[height];
+    for (i=0; i<height; i++){
+        mat_brightness[i] = (double*)malloc(sizeof(double)*width);
+        mat_is_visited[i] = (int*)calloc(width, sizeof(int));
+    }
+    for (i=0; i<height; i++){
+        for (j=0; j<width; j++){
+            scanf("%lf", mat_brightness[i]+j);
+        }
+    }
+    scoop_the_blob(mat_brightness, mat_is_visited, 0.5, blob_size, 0, 0, 0, width, height);
+    for (i=0; i<height; i++){
+        for (j=0; j<width; j++){
+            printf("%d ", mat_is_visited[i][j]);
+        }
+        printf("\n");
+    }
+    printf("%d\n", blob_size[0]);
+    for (i=0; i<width; i++){
+        free(mat_brightness[i]);
+        free(mat_is_visited[i]);
+    }
+    free(blob_size);
+    return 0;
+}
